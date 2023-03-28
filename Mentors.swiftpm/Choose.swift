ee1c3c470559a6224bd36179ee20e482cba0eb52
JAESEOK LEE <poal5642@snu.ac.kr>
2023-03-28 11:39:39 +0900

    Recording Page UI Development

---
+++ Mentors.swiftpm/Choose.swift
@@ -0,0 +1,20 @@
+//
+//  Choose.swift
+//  Mentors
+//
+//  Created by JAESEOK LEE on 2023/03/27.
+//
+
+import SwiftUI
+
+struct Choose: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Choose_Previews: PreviewProvider {
+    static var previews: some View {
+        Choose()
+    }
+}
