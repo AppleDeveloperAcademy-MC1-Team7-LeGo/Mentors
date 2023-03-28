bc0aa8bec637ef7ec5beebe1a8920f66ffdfbd2e
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-28 18:05:50 +0900

    Sending3 생성 및 디자인 배치

---
+++ Mentors.swiftpm/Sending3.swift
@@ -0,0 +1,63 @@
+//
+//  Sending3.swift
+//  Mentors
+//
+//  Created by 손서연 on 2023/03/28.
+//
+
+import SwiftUI
+
+struct Sending3: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var btnBack : some View {
+        Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+            
+        }) {
+        HStack {
+            Image(systemName: "chevron.backward")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            }
+        }
+    }
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("어떤 멘토링이 필요하신가요?")
+                .font(.system(size: 24, weight: .bold))
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                .padding(.top, 40)
+            Text("공백 포함 50자 이상으로 자세하게 적어주세요.")
+                .font(.system(size: 14, weight: .bold))
+                .fontWeight(.semibold)
+                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                .padding(.top, 4)
+            MentoringTextField()
+                .padding(.top, 16)
+            
+            Spacer()
+            
+           
+            
+            Nextbutton(title: "다음", isSelected: false, action: {print("next")})
+                .padding(.bottom, 21)
+        }
+        .padding(.horizontal, 26)
+        
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
+        
+        .background(Color.init(hex: "F9F9F9"))
+    }
+}
+
+struct Sending3_Previews: PreviewProvider {
+    static var previews: some View {
+        Sending3()
+    }
+}
