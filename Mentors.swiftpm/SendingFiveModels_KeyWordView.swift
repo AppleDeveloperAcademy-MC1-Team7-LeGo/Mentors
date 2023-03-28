5652eedffbb19a42be409386444953bbcfa92dbc
GaOn <xnoag@icloud.com>
2023-03-28 10:49:34 +0900

    SendingFiveView_KeyWordViewAndModels

--- Mentors.swiftpm/SendingFiveModels_KeyWordView.swift
+++ Mentors.swiftpm/SendingFiveModels_KeyWordView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct SendingFiveModels_KeyWordView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("# \(keyWordArray[0].keyWord)")
+            .font(.custom("Apple SD Gothic Neo", size: 18))
+            .fontWeight(.semibold)
+            .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            .padding()
+            .background(keyWordArray[0].backgroundColor)
+            .cornerRadius(15)
+        }
     }
-}
+
 
 struct SendingFiveModels_KeyWordView_Previews: PreviewProvider {
     static var previews: some View {
