117c6f22e7c6ed0882f4016c5faf72a5e8ec39fc
GaOn <xnoag@icloud.com>
2023-03-28 10:49:09 +0900

    SendingFiveView_KeyWordViewAndModels

---
+++ Mentors.swiftpm/SendingFiveView.swift
@@ -0,0 +1,20 @@
+//
+//  SendingFiveView.swift
+//  Mentors
+//
+//  Created by xnoag on 2023/03/28.
+//
+
+import SwiftUI
+
+struct SendingFiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SendingFiveView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendingFiveView()
+    }
+}
