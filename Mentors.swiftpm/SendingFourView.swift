abce2f42391ed2acd080b368f7aec7fa0b6a7952
GaOn <xnoag@icloud.com>
2023-03-28 16:56:44 +0900

    SendingFourView_Finished

--- Mentors.swiftpm/SendingFourView.swift
+++ Mentors.swiftpm/SendingFourView.swift
@@ -15,17 +15,17 @@ struct SendingFourView: View {
             
             // VStack-Text
             Text("멘토가 보면 좋을 파일이 있다면,\rPDF 또는 사진으로 올려주세요")
-            .font(.custom("Apple SD Gothic Neo", size: 24))
-            .fontWeight(.bold)
-            .foregroundColor(Color("CustomBlack"))
-            .multilineTextAlignment(.leading)
-            .padding(.bottom, 10)
-
+                .font(.custom("Apple SD Gothic Neo", size: 24))
+                .fontWeight(.bold)
+                .foregroundColor(Color(hex: "292929"))
+                .multilineTextAlignment(.leading)
+                .padding(.bottom, 10)
+            
             // VStack-Text
             Text("최대 10개의 파일")
                 .font(.custom("Apple SD Gothic Neo", size: 16))
                 .fontWeight(.regular)
-                .foregroundColor(Color("CustomBlack"))
+                .foregroundColor(Color(hex: "292929"))
                 .padding(.bottom, 24)
             
             // VStack-HStack
@@ -35,124 +35,106 @@ struct SendingFourView: View {
                 Button(action: {}) {
                     VStack {
                         Image(systemName: "square.and.arrow.up.fill")
-                            .foregroundColor(Color("CustomJjinYellow"))
+                            .foregroundColor(Color(hex: "F6D555"))
                             .padding(.bottom, 7)
                         Text("파일 또는 사진 첨부")
                             .font(.custom("Apple SD Gothic Neo", size: 14))
                             .fontWeight(.regular)
-                            .foregroundColor(Color("CustomBlack"))
+                            .foregroundColor(Color(hex: "292929"))
                             .fixedSize()
                     }
                 }
                 .fixedSize()
                 .frame(width: 140, height: 94)
-                .background(Color("CustomYellow"))
+                .background(Color(hex: "fdf4d1"))
                 .cornerRadius(10)
+                
+                // VStack-HStack-Spacer
+                Spacer()
                 
                 // VStack-HStack-Button
                 Button(action: {}) {
                     VStack {
                         Image(systemName: "x.square.fill")
-                            .foregroundColor(Color("CustomJjinYellow"))
+                            .foregroundColor(Color(hex: "E5E2D7"))
                             .padding(.bottom, 7)
                         Text("첨부하지 않음")
                             .font(.custom("Apple SD Gothic Neo", size: 14))
                             .fontWeight(.regular)
-                            .foregroundColor(Color("CustomBlack"))
+                            .foregroundColor(Color(hex: "292929"))
                             .fixedSize()
                     }
                 }
                 .fixedSize()
                 .frame(width: 140, height: 94)
-                .background(Color("CustomJjinGrey"))
+                .background(Color(hex: "F4F4F0"))
                 .cornerRadius(10)
                 
-                
-                
-                // 간격 부여
-                Spacer()
-                
-                // 우 Image
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 140, height: 94)
-                        .foregroundColor(Color(red: 0.9568627450980393, green: 0.9568627450980393, blue: 0.9411764705882353))
-                        
-                    
-                    VStack {
-                        Image(systemName: "x.square.fill")
-                            .foregroundColor(Color(red: 0.8980392156862745, green: 0.8862745098039215, blue: 0.8431372549019608))
-                            .padding(.bottom, 7)
-                        Text("첨부하지 않음")
-                            .font(.custom("Apple SD Gothic Neo", size: 14))
-                            .fontWeight(.regular)
-                            .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
-                    }
-                }
             }
             .padding(.bottom, 24)
             
-            // Image, Text 3개 VStack
-            VStack {
-                HStack {
-                    ZStack {
-                        Circle()
-                            .frame(width: 18, height: 18)
-                            .foregroundColor(Color(red: 0.9921568627450981, green: 0.9568627450980393, blue: 0.8196078431372549))
-                        Image(systemName: "paperclip")
-                            .resizable()
-                            .frame(width: 12, height: 12)
-                    }
-                    Text("파일명")
-                        .font(.custom("Apple SD Gothic Neo", size: 12))
-                        .fontWeight(.regular)
-                        .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            // VStack-HStack
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 18, height: 18)
+                        .foregroundColor(Color(hex: "FDF4D1"))
+                    Image(systemName: "paperclip")
+                        .resizable()
+                        .frame(width: 12, height: 12)
                 }
-                .padding(.bottom, 8)
-                
-                HStack {
-                    ZStack {
-                        Circle()
-                            .frame(width: 18, height: 18)
-                            .foregroundColor(Color(red: 0.9921568627450981, green: 0.9568627450980393, blue: 0.8196078431372549))
-                        Image(systemName: "paperclip")
-                            .resizable()
-                            .frame(width: 12, height: 12)
-                    }
-                    Text("파일명")
-                        .font(.custom("Apple SD Gothic Neo", size: 12))
-                        .fontWeight(.regular)
-                        .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                Text("파일명")
+                    .font(.custom("Apple SD Gothic Neo", size: 12))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(hex: "292929"))
+            }
+            .padding(.bottom, 8)
+            
+            // VStack-HStack
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 18, height: 18)
+                        .foregroundColor(Color(hex: "FDF4D1"))
+                    Image(systemName: "paperclip")
+                        .resizable()
+                        .frame(width: 12, height: 12)
                 }
-                .padding(.bottom, 8)
-                
-                HStack {
-                    ZStack {
-                        Circle()
-                            .frame(width: 18, height: 18)
-                            .foregroundColor(Color(red: 0.9921568627450981, green: 0.9568627450980393, blue: 0.8196078431372549))
-                        Image(systemName: "paperclip")
-                            .resizable()
-                            .frame(width: 12, height: 12)
-                    }
-                    Text("파일명")
-                        .font(.custom("Apple SD Gothic Neo", size: 12))
-                        .fontWeight(.regular)
-                        .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                Text("파일명")
+                    .font(.custom("Apple SD Gothic Neo", size: 12))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(hex: "292929"))
+            }
+            .padding(.bottom, 8)
+            
+            // VStack-HStack
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 18, height: 18)
+                        .foregroundColor(Color(hex: "FDF4D1"))
+                    Image(systemName: "paperclip")
+                        .resizable()
+                        .frame(width: 12, height: 12)
                 }
+                Text("파일명")
+                    .font(.custom("Apple SD Gothic Neo", size: 12))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(hex: "292929"))
             }
             .padding(.bottom, 313)
         
-            // Image
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 338, height: 48).foregroundColor(Color(red: 0.9647058823529412, green: 0.8352941176470589, blue: 0.3333333333333333))
+            // VStack-Button
+            Button(action:{}) {
                 Text("다음")
                     .font(.custom("Apple SD Gothic Neo", size: 18))
                     .fontWeight(.semibold)
-                    .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                    .foregroundColor(Color(hex: "292929"))
             }
-                
+            .fixedSize()
+            .frame(width: 338, height: 48)
+            .background(Color(hex: "F6D555"))
+            .cornerRadius(10)
         }
         .padding()
     }
@@ -163,3 +145,8 @@ struct SendingFourView_Previews: PreviewProvider {
         SendingFourView()
     }
 }
+
+
+
+ // 1. Button 비슷한 틀을 한 번에 묶을 수 있는지
+ // 2. Button Click Animation 효과시 색상 변경
