25f5fc7ee6e6de944cdbdd16ff4199853d1aa5f3
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-28 11:05:07 +0900

    TextField 설정 완료

--- Mentors.swiftpm/MentorsTextField.swift
+++ Mentors.swiftpm/MentorsTextField.swift
@@ -12,15 +12,25 @@ struct MentorsTextField: View {
     
     
     var body: some View {
-        TextField("팀 이름을 정확하게 적어주세요", text: $text)
-            .padding()
-            .background(Color.gray)
-            .cornerRadius(10)
-            .foregroundColor(.white)
-            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
-            .padding(.horizontal)
-            .multilineTextAlignment(.center)
-            .keyboardType(.default)
+        ZStack {
+            TextField("", text: $text)
+                .placeholder(when: text.isEmpty, placeholder: {
+                    Text("팀 이름을 정확하게 적어주세요")
+                        .foregroundColor(Color.init(hex: "AAA48E"))
+                })
+                .padding(.horizontal, 6)
+                .foregroundColor(Color.init(hex: "292929"))
+                .font(.system(size: 16, weight: .medium))
+                .frame(height: 34)
+                .overlay(alignment: .bottom) {
+                    Capsule()
+                        .foregroundColor(Color.init(hex: "F6D555"))
+                        .frame(height: 1.2)
+                }
+        }
+//        .padding(.horizontal, 26)
+//        .frame(width: UIScreen.main.bounds.width - 26)
+
     }
 }
 
