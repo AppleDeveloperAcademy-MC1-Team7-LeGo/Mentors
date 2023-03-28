12ae7afd89d0533e06a9fc563a8ce71a83c9b70d
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-28 18:07:45 +0900

    Sending2 디자인, 텍스트필드 배치

--- Mentors.swiftpm/Sending2.swift
+++ Mentors.swiftpm/Sending2.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct Sending2: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var btnBack : some View {
+        Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+            
+        }) {
+        HStack {
+            Image(systemName: "chevron.backward")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            }
+        }
+    }
+    
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("글의 제목을 적어주세요")
                 .font(.system(size: 24, weight: .bold))
+                .fontWeight(.bold)
                 .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                .padding(.top, 40)
+            MentorsTextField()
+                .padding(.top, 20)
+            
+            Spacer()
+            
+            Nextbutton(title: "다음", isSelected: false, action: {print("next")})
+                .padding(.bottom, 418)
         }
+        .padding(.horizontal, 26)
         
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
+        
+        .background(Color.init(hex: "F9F9F9"))
     }
 }
 
