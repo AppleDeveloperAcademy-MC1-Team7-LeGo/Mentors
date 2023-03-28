e72fcf1fd1dcee947b075c6d77719dced41a5d0b
JAESEOK LEE <poal5642@snu.ac.kr>
2023-03-28 11:39:39 +0900

    Recording Page UI Development

---
+++ Mentors.swiftpm/Check.swift
@@ -0,0 +1,20 @@
+//
+//  Check.swift
+//  Mentors
+//
+//  Created by JAESEOK LEE on 2023/03/27.
+//
+
+import SwiftUI
+
+struct Check: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Check_Previews: PreviewProvider {
+    static var previews: some View {
+        Check()
+    }
+}
