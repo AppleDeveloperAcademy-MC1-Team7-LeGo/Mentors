0b3357e93f72be5e9191514f3cab23f150323c03
GaOn <xnoag@icloud.com>
2023-03-28 12:39:39 +0900

    SendingFiveView

--- Mentors.swiftpm/SendingFiveModels_KeyWordView.swift
+++ Mentors.swiftpm/SendingFiveModels_KeyWordView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct SendingFiveModels_KeyWordView: View {
     
     var body: some View {
-        Text("# \(keyWordArray[0].keyWord)")
+        Text("# \(keyWordArray[2].keyWord)")
             .font(.custom("Apple SD Gothic Neo", size: 18))
             .fontWeight(.semibold)
             .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
-            .padding()
-            .background(keyWordArray[0].backgroundColor)
+            .padding(12)
+            .background(keyWordArray[2].backgroundColor)
             .cornerRadius(15)
+            .fixedSize()
         }
     }
 
--- Mentors.swiftpm/SendingFiveView.swift
+++ Mentors.swiftpm/SendingFiveView.swift
@@ -24,8 +24,85 @@ struct SendingFiveView: View {
                 .font(.custom("Apple SD Gothic Neo", size: 14))
                 .fontWeight(.regular)
                 .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
-                .padding(.bottom, 24)
+                .padding(.bottom, 40)
+            
+            // KeyWords
+            VStack(alignment: .leading) {
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+                .padding(.bottom, 4)
+                
+                HStack {
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                    SendingFiveModels_KeyWordView().padding(.trailing, 3)
+                }
+            }
+            .padding(.bottom, 40)
+            
+            // Image
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 338, height: 48).foregroundColor(Color(red: 0.9647058823529412, green: 0.8352941176470589, blue: 0.3333333333333333))
+                Text("다음")
+                    .font(.custom("Apple SD Gothic Neo", size: 18))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            }
+            
         }
+        .padding()
     }
 }
 
