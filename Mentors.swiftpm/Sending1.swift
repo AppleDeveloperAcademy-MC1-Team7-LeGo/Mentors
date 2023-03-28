1442b2f80d60cf432c0af2bff32cb4d0a3cb45bf
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-28 09:56:05 +0900

    버튼 코드 변경
    button(action label) -> enum 타입을 생성하여 코드 변경 및 눌렀을 때 색 변경

--- Mentors.swiftpm/Sending1.swift
+++ Mentors.swiftpm/Sending1.swift
@@ -7,51 +7,82 @@
 
 import SwiftUI
 
-
-
-struct Sending1: View {
-    var body: some View {
-            VStack(alignment: .leading) {
-                Text("누가 보내는 사연인가요?")
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
-                
-                HStack(spacing: 18) {
-                    Button {
-                        
-                    } label: {
-                        Text("팀")
-                            .font(.system(size: 18, weight: .semibold))
-                            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 48)
-                            .background(Color(hex: "E5E2D7"))
-                            .foregroundColor(Color(hex: "F9F9F9"))
-                            .cornerRadius(10)
-                    }
-                    
-                    Button {
-                        
-                    } label: {
-                        Text("개인")
-                            .font(.system(size: 18, weight: .semibold))
-                            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 48)
-                            .background(Color(hex: "E5E2D7"))
-                            .foregroundColor(Color(hex: "F9F9F9"))
-                            .cornerRadius(10)
-                    }
-
-                }
-                
-                
-                Spacer()
-            }
-    }
-   
-    
-    struct sending1_Previews: PreviewProvider {
-        static var previews: some View {
-            Sending1()
-        }
-    }
+enum SelectedButtonType {
+    case team
+    case personal
+    case none
 }
 
 
+//NavigationView {
+//    Text("hello")
+//        .navigationBarTitleDisplayMode(.inline)
+//        .toolbar {
+//            ToolbarItem(placement: .principal) {
+//                HStack {
+//                    Image(systemName: "chevron.backward")
+//                }
+//            }
+//        }
+//}
+
+struct Sending1: View {
+    @State private var selectButton: SelectedButtonType = .none
+    @State private var teamName: String = .init()
+
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("누가 보내는 사연인가요?")
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            
+            HStack(spacing: 18) {
+                SelectableButton(
+                    title: "팀", isSelected: selectButton == .team) {
+                        selectButton = .team
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 2 - 26 - 9)
+                
+                SelectableButton(
+                    title: "개인", isSelected: selectButton == .personal) {
+                        selectButton = .personal
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 2 - 26 - 9)
+                        
+                        
+                        //                    Button {
+                        //
+                        //                    } label: {
+                        //                        Text("팀")
+                        //                            .font(.system(size: 18, weight: .semibold))
+                        //                            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 48)
+                        //                            .background(Color(hex: "E5E2D7"))
+                        //                            .foregroundColor(Color(hex: "F9F9F9"))
+                        //                            .cornerRadius(10)
+                        //                    }
+                        //
+                        //                    Button {
+                        //
+                        //                    } label: {
+                        //                        Text("개인")
+                        //                            .font(.system(size: 18, weight: .semibold))
+                        //                            .frame(width: UIScreen.main.bounds.width / 2 - 26, height: 48)
+                        //                            .background(Color(hex: "E5E2D7"))
+                        //                            .foregroundColor(Color(hex: "F9F9F9"))
+                        //                            .cornerRadius(10)
+                        //                    }
+                        
+                    }
+                Spacer()
+            }
+        }
+        
+        
+        struct sending1_Previews: PreviewProvider {
+            static var previews: some View {
+                Sending1()
+            }
+        }
+    }
