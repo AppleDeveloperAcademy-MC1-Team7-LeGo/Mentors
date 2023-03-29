e0b7e47f83f766e8e520709c096e9bda39b30c29
dayo2n <0217dayun@naver.com>
2023-03-29 12:18:45 +0900

    [FEAT] 리이오와 멘토링 화면(동영상 첨부식) 구현

---
+++ Mentors.swiftpm/MentoringWithLeeo.swift
@@ -0,0 +1,27 @@
+//
+//  MentoringWithLeeo.swift
+//  Mentors
+//
+//  Created by 제나 on 2023/03/29.
+//
+
+import SwiftUI
+import AVKit
+
+struct MentoringWithLeeo: View {
+    
+    let avPlayer = AVPlayer(url:  Bundle.main.url(forResource: "video", withExtension: "MOV")!)
+    
+    var body: some View {
+        VideoPlayer(player: avPlayer)
+            .onDisappear {
+                avPlayer.isMuted = false
+            }
+    }
+}
+
+struct MentoringWithLeeo_Previews: PreviewProvider {
+    static var previews: some View {
+        MentoringWithLeeo()
+    }
+}
