7bb105b2a1e8550723e52a7cddaf1bc17edfd618
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-29 16:28:20 +0900

    home 작업 중
    1. 멘토링사연보내기 버튼 -> 다음 뷰와 연결 완료
    
    2. 진행 중인 멘토링 기록하기 가로 너비가 이상함. + 멘토링 시간이 됐을 때 버튼이 생성되도록 만들어야 함.
    
    3. 탭바에서 홈이 기본값으로 설정되어야 하는데 안 됨.

--- Mentors.swiftpm/Home.swift
+++ Mentors.swiftpm/Home.swift
@@ -12,21 +12,104 @@ struct Home: View {
     
     var body: some View {
         NavigationView {
-            NavigationLink(isActive: $isNavigate) {
-                Sending1()
-            } label: {
-                Button {
-                    isNavigate = true
+            VStack(alignment: .leading) {
+                Text("러너 님,\n오늘도 챌린지 힘내세요☺️")
+                    .font(Font.custom("Apple SD Gothic Neo", size: 28))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+                    .padding(.top, 44)
+                
+                //진행 중인 멘토링 기록하기
+                //왜..가로가 안 넓어질까요?
+                    Button {
+                    
                 } label: {
-                    Text("다음 화면으로")
-                        .fontWeight(.semibold)
-                        .frame(width: 200, height: 100)
-                        .cornerRadius(8)
-                        .border(Color.blue, width: 1)
+                    Text("진행 중인 멘토링 기록하기")
+                        .fontWeight(.bold)
+                        .frame(height: 48)
+                        .background(Color.init(hex: "E8585E"))
+                        .foregroundColor(Color.init(hex: "f9f9f9"))
+                        
                 }
+                .padding(.top, 40)
+                .padding(.horizontal, 26)
+                
+                //내비게이션
+                NavigationLink(isActive: $isNavigate) {
+                    Sending1()
+                } label: {
+                    HomeMentoringButton(title: "내 멘토링 사연 보내기", isSelected: true, action: {
+                        isNavigate = true
+                    })
+                    .frame(width: UIScreen.main.bounds.width - 52, height: 100)
+                    .cornerRadius(10)
+                    .padding(.top, 66)
+                }
+                NavigationLink(isActive: $isNavigate) {
+                    Sending1()
+                } label: {
+                    HomeMentoringButton(title: "내 멘토링 기록 모아보기", isSelected: true, action: {
+                        isNavigate = true
+                    })
+                    .frame(width: UIScreen.main.bounds.width - 52, height: 100)
+                    .cornerRadius(10)
+                    .padding(.top, 30)
+                }
+                Spacer()
+                
+                //탭바
+                TabView {
+                    Text("")
+                        .tabItem {
+                            Image(systemName: "doc.plaintext")
+                        }
+                    Text("")
+                        .tabItem {
+                            Image(systemName: "square.on.square.badge.person.crop")
+                        }
+                    Text("")
+                        .tabItem {
+                            Image(systemName: "house")
+                        }
+                    Text("")
+                        .tabItem {
+                            Image(systemName: "ellipsis.message")
+                        }
+                    Text("")
+                        .tabItem {
+                            Image(systemName: "person.circle")
+                        }
+                }
+                
             }
+            .padding(.horizontal, 26)
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    //    spending1로 연결되는 내비게이션뷰
+    //    {
+    //        NavigationView {
+    //            NavigationLink(isActive: $isNavigate) {
+    //                Sending1()
+    //            } label: {
+    //                Button {
+    //                    isNavigate = true
+    //                } label: {
+    //                    Text("다음 화면으로")
+    //                        .fontWeight(.semibold)
+    //                        .frame(width: 200, height: 100)
+    //                        .cornerRadius(8)
+    //                        .border(Color.blue, width: 1)
+    //                }
+    //            }
+    //        }
+    //    }
 }
 
 struct home_Previews: PreviewProvider {
