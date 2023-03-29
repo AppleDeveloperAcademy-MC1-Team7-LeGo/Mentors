bd4df80ef463ef6cfcdeaca86f2dced007f76c9b
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-29 16:23:03 +0900

    Sending1 완료

--- Mentors.swiftpm/Sending1.swift
+++ Mentors.swiftpm/Sending1.swift
@@ -14,74 +14,98 @@ enum SelectedButtonType {
 }
 
 
-//NavigationView {
-//    Text("hello")
-//        .navigationBarTitleDisplayMode(.inline)
-//        .toolbar {
-//            ToolbarItem(placement: .principal) {
-//                HStack {
-//                    Image(systemName: "chevron.backward")
-//                }
-//            }
-//        }
-//}
 
 struct Sending1: View {
     @State private var selectButton: SelectedButtonType = .none
     @State private var teamName: String = .init()
+    @State private var nextButtonDisabled = true
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var btnBack : some View {
         Button(action: {
-        self.presentationMode.wrappedValue.dismiss()
+            self.presentationMode.wrappedValue.dismiss()
             
         }) {
-        HStack {
-            Image(systemName: "chevron.backward")
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
+            HStack {
+                Image(systemName: "chevron.backward")
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
             }
         }
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             Text("누가 보내는 사연인가요?")
                 .font(Font.custom("Apple SD Gothic Neo", size: 24))
+                .fontWeight(.bold)
                 .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
                 .padding(.top, 40)
+                .padding(.bottom, 37)
             
             HStack(spacing: 18) {
                 SelectableButton(
                     title: "팀", isSelected: selectButton == .team) {
-                        selectButton = .team
+                        withAnimation(.easeIn(duration: 0.2)) {
+                            selectButton = .team
+                        }
                     }
-                //                    .frame(width: UIScreen.main.bounds.width / 2 - 26 - 9)
                 
                 SelectableButton(
                     title: "개인", isSelected: selectButton == .personal) {
-                        selectButton = .personal
+                        withAnimation(.easeIn(duration: 0.2)) {
+                            selectButton = .personal
+                        }
                     }
-                //                    .frame(width: UIScreen.main.bounds.width / 2 - 26 - 9)
             }
+            
             if selectButton == .team {
-                MentorsTextField()
+                MentorsTextField(text: $teamName)
                     .padding(.top, 20)
+                    .padding(.bottom, 80)
+            }
+            
+            if selectButton != .none {
+                Nextbutton(title: "다음", isAbled: selectButton == .personal || selectButton == .team && teamName != "") {
+                    print("next")
+                }
+                .padding(.top, selectButton == .personal ? 134 : 0)
+                .disabled(nextButtonDisabled)
             }
             Spacer()
         }
         .padding(.horizontal, 26)
-        
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: btnBack)
-        
         .background(Color.init(hex: "F9F9F9"))
-        
     }
 }
-                    
-                    
+
+//다음 뷰로 넘어가는거 아직 못함
+
+struct MovingOnNextView: View {
+    @State private var text: String = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Enter text", text: $text)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("Next") {
+                navigateToNextView()
+            }
+        }
+    }
+    
+    private func navigateToNextView() {
+        // Code to navigate to next view
+    }
+}
+
+//하단 다음 버튼
+
+
 struct sending1_Previews: PreviewProvider {
     static var previews: some View {
         Sending1()
