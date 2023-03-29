1a7b2481fc020d6222606240ff451016383caac3
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-29 12:14:37 +0900

    Extension 폴더 생성 및 위치 이동

--- Mentors.swiftpm/BtnExtension/HomeMentoringButton.swift
+++ Mentors.swiftpm/BtnExtension/HomeMentoringButton.swift
@@ -1,13 +1,14 @@
 //
-//  Nextbutton.swift
+//  HomeMentoringButton.swift
 //  Mentors
 //
-//  Created by 손서연 on 2023/03/28.
+//  Created by 손서연 on 2023/03/29.
 //
 
 import SwiftUI
 
-struct Nextbutton: View {
+struct HomeMentoringButton: View {
+    
     var title: String
     var isSelected: Bool
     var action: () -> Void
@@ -29,26 +30,25 @@ struct Nextbutton: View {
             HStack {
                 Spacer()
                 Text(title)
-                    .font(Font.custom("Apple SD Gothic Neo", size: 18))
-                    .fontWeight(.semibold)
-                
+                    .font(Font.custom("Apple SD Gothic Neo", size: 24))
+                    .fontWeight(.bold)
                 Spacer()
             }
-            .frame(height: 48)
-            .background(isSelected ? Color.init(hex: "F6D555") : Color.init(hex: "E5E2D7"))
-            .foregroundColor(isSelected ? Color.init(hex: "292929") : Color.init(hex: "F9F9F9"))
+            .frame(height: 156)
+            .background(isSelected ? Color.init(hex: "F6D555") : Color.init(hex: "FDF4D1"))
+            .foregroundColor(isSelected ? Color.init(hex: "292929") : Color.init(hex: "292929"))
             .cornerRadius(10)
             .overlay{
                 RoundedRectangle(cornerRadius: 10)
-                    .stroke(isSelected ? Color.init(hex: "F6D555") : Color.init(hex: "E5E2D7"), lineWidth: 1.2)
+                    .stroke(isSelected ? Color.init(hex: "F6D555") : Color.init(hex: "FDF4D1"), lineWidth: 1.2)
             }
         }
     }
 }
 
-struct Nextbutton_Previews: PreviewProvider {
+struct HomeMentoringButton_Previews: PreviewProvider {
     static var previews: some View {
-        Nextbutton(title: "다음", isSelected: false) {
+        HomeMentoringButton(title: "", isSelected: false) {
             print("")
         }
     }
