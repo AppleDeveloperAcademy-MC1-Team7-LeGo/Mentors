dbb2b8795cdd43474d63cf1df71a2a6775a4170c
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-29 19:18:20 +0900

    MentorsTextField 프리뷰 설정
    1. @Binding -> @State로 변경
    2. Sending1 65번줄
    : MentorsTextField(text: $teamName) -> MentorsTextField(text: teamName)로 변경

--- Mentors.swiftpm/TxtExtension/MentorsTextField.swift
+++ Mentors.swiftpm/TxtExtension/MentorsTextField.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct MentorsTextField: View {
-    @Binding var text: String
+    @State var text: String
     
     
     var body: some View {
         ZStack {
             TextField("", text: $text)
                 .placeholder(when: text.isEmpty, placeholder: {
-                    //아래 Text("" <-요 부분에 뭘 넣어야 하죠?? placeholder가 sending1과 sending2 각각 달라야 해요
+                    //아래 Text("") <-요 부분에 뭘 넣어야 하죠?? placeholder가 sending1과 sending2 각각 달라야 해요
                     Text("")
                         .fontWeight(.medium)
                         .foregroundColor(Color.init(hex: "AAA48E"))
@@ -30,14 +30,16 @@ struct MentorsTextField: View {
                         .frame(height: 1.2)
                 }
         }
-//        .padding(.horizontal, 26)
-//        .frame(width: UIScreen.main.bounds.width - 26)
-
+        //        .padding(.horizontal, 26)
+        //        .frame(width: UIScreen.main.bounds.width - 26)
+        
     }
 }
-//
-//struct MentorsTextField_Previews: PreviewProvider {
-//    static var previews: some View {
-////         MentorsTextField(text: "")
-//    }
-//}
+
+
+struct MentorsTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        MentorsTextField(text: "")
+//         MentorsTextField(text: "")
+    }
+}
