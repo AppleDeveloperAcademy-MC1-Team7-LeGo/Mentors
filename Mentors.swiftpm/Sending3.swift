68b77f10c88c01847ddd17847d76807f68da4ec2
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-30 19:09:45 +0900

    Sending3 내비게이션, 글자수 설정

--- Mentors.swiftpm/Sending3.swift
+++ Mentors.swiftpm/Sending3.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct Sending3: View {
     
     @State private var text = ""
-    @State private var isNextButtonEnabled = false
+    @State private var navigationIsActive: Bool = false
+
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var btnBack : some View {
         Button(action: {
             self.presentationMode.wrappedValue.dismiss()
-            
         }) {
             HStack {
                 Image(systemName: "chevron.backward")
@@ -35,27 +35,31 @@ struct Sending3: View {
                 .fontWeight(.bold)
                 .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
                 .padding(.top, 40)
+            
             Text("공백 포함 50자 이상으로 자세하게 적어주세요.")
                 .font(.system(size: 14, weight: .bold))
                 .fontWeight(.semibold)
                 .foregroundColor(Color(red: 0.1607843137254902, green: 0.1607843137254902, blue: 0.1607843137254902))
                 .padding(.top, 4)
-            MentoringTextField()
+            
+            MentoringTextField(text: $text)
                 .padding(.top, 16)
-            //TODO: 공백 포함 최소 50자 이상으로 적어야 '다음'버튼이 활성화가 되어야 합니다
-                .onChange(of: text) { newValue in
-                    isNextButtonEnabled = newValue.count >= 50
-                }
-                    
+            
             Spacer()
             
-            NavigationLink(destination: SendingFourView()) {
-                Nextbutton(title: "다음", isAbled: true, action: {print("next")})
-                    .padding(.bottom, 21)
-                    .navigationBarBackButtonHidden(true)
+            
+            Nextbutton(title: "다음", isAbled: text.count >= 50) {
+                navigationIsActive = true
             }
+            .padding(.bottom, 21)
         }
         .padding(.horizontal, 26)
         .background(Color.init(hex: "F9F9F9"))
+        .background(EmptyNavigationLink(isActive: $navigationIsActive, {
+            SendingFourView()
+        }))
+
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
     }
 }
