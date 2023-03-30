c68eb3f9d4a1a64fc76f741933c7a7e03bb38d4b
Dayeon Moon <0217dayun@naver.com>
2023-03-30 12:28:53 +0900

    [FEAT] 리이오와 멘토링 UI 구현

--- Mentors.swiftpm/MentoringWithLeeo.swift
+++ Mentors.swiftpm/MentoringWithLeeo.swift
@@ -13,10 +13,30 @@ struct MentoringWithLeeo: View {
     let avPlayer = AVPlayer(url:  Bundle.main.url(forResource: "Mentoring", withExtension: "mp4")!)
     
     var body: some View {
-        VideoPlayer(player: avPlayer)
-            .onDisappear {
-                avPlayer.isMuted = false
+        if #available(iOS 16.0, *) {
+            VStack {
+                Spacer()
+                
+                VideoPlayer(player: avPlayer)
+                    .onDisappear {
+                        avPlayer.isMuted = false
+                    }
+                
+                Spacer()
             }
+            .ignoresSafeArea()
+            .background(Color(.black))
+            .onAppear {
+                avPlayer.play()
+            }
+            .onDisappear {
+                avPlayer.pause()
+                avPlayer.seek(to: .zero)
+            }
+            .toolbar(.hidden, for: .tabBar)
+        } else {
+            // Fallback on earlier versions
+        }
     }
 }
 
