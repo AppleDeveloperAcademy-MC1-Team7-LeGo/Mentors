ee37383e83fc3d3219a25bae44efb80210f70fe3
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-30 19:06:13 +0900

    MentorsTextField @State 제거

--- Mentors.swiftpm/TxtComponent/MentorsTextField.swift
+++ Mentors.swiftpm/TxtComponent/MentorsTextField.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct MentorsTextField: View {
     @Binding var text: String
-    @State var placeholder: String
+    var placeholder: String
     
     var body: some View {
         ZStack {
             TextField("", text: $text)
                 .placeholder(when: text.isEmpty, placeholder: {
-                    // 아래 Text("") <-요 부분에 뭘 넣어야 하죠?? placeholder가 sending1과 sending2 각각 달라야 해요 → 해결! Notion 참고
                     Text("\(placeholder)")
                         .fontWeight(.medium)
                         .foregroundColor(Color.init(hex: "AAA48E"))
