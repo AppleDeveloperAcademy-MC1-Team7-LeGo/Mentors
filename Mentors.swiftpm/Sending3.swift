fcdc34b6f857d9c069f5b8ce379b4baa0a130217
sohnseoyeon <63771212+sohnseoyeon@users.noreply.github.com>
2023-03-30 20:22:27 +0900

    Sending3 자동 텍스트 나오는 기능
    '다음'버튼 말고 '텍스트에디터'의 프레임을 누르면 나오도록 설정함.

--- Mentors.swiftpm/Sending3.swift
+++ Mentors.swiftpm/Sending3.swift
@@ -11,7 +11,7 @@ struct Sending3: View {
     
     @State private var text = ""
     @State private var navigationIsActive: Bool = false
-
+    
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
@@ -43,23 +43,32 @@ struct Sending3: View {
                 .padding(.top, 4)
             
             MentoringTextField(text: $text)
+                .onTapGesture {
+                    if text.isEmpty {
+                        text = "안녕하세요 Leeo!\nMC1 오전 세션 Group3 Team7 LeGo 입니다.\n\n저희는 어제 발표가 끝남과 동시에 MC1 전반부가 마무리되었기 때문에 전반부에 대한 회고를 진행하려고 합니다.\n그런데 팀 회고가 처음이다 보니 어떻게 시작해야 할지 감이 잘 오지 않습니다.\n\n이에 멘토링을 요청합니다. 팀 회고를 어떻게 시작하고 어떻게 진행해야 하는지에 대해 도움을 주시면 감사하겠습니다."
+                    }
+                }
                 .padding(.top, 16)
                 .padding(.bottom, 26)
             
             Spacer()
             
-            
             Nextbutton(title: "다음", isAbled: text.count >= 50) {
                 navigationIsActive = true
             }
-            .padding(.bottom, 55)
         }
+        .padding(.bottom, 55)
         .padding(.horizontal, 26)
         .background(Color.init(hex: "F9F9F9"))
         .background(EmptyNavigationLink(isActive: $navigationIsActive, {
             SendingFourView()
+            // 버튼 최소 50자 이상 나오도록 하는 코드
+            //            Nextbutton(title: "다음", isAbled: text.count >= 50) {
+            //                navigationIsActive = true
+            //            }
+            
         }))
-
+        
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: btnBack)
     }
